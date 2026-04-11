f83b9f68b3243680ed4de8e0c0a698a81aad2dcb
Glebiko <kiddylove96@gmail.com>
2026-04-11 17:54:13 +0300

    new button activities

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,10 @@ const headerBtn = document.querySelector('.header__btn');
 const headerMenu = document.querySelector('.header__menu');
 const openModalBtn = document.querySelector('.open-modal');
 const modal = document.querySelector('.modal');
+const footerModal = document.querySelector('.footer__modal');
 const modalOverlay = document.querySelector('.modal__overlay');
+const footerModalOverlay = document.querySelector('.footer__modal-overlay');
+const footerDonate = document.querySelector('.footer__donate');
 
 const initLogoAnimation = () => {
   const container = document.querySelector('.main__shop-logo');
@@ -70,6 +73,18 @@ function closeModal() {
   body.classList.remove('scroll-lock');
 }
 
+function openFooterModal() {
+  if (!modal) return;
+  footerModal.classList.add('is--open');
+  body.classList.add('scroll-lock');
+}
+
+function closeFooterModal() {
+  if (!modal) return;
+  footerModal.classList.remove('is--open');
+  body.classList.remove('scroll-lock');
+}
+
 
 document.addEventListener('DOMContentLoaded', initLogoAnimation);
 
@@ -80,9 +95,14 @@ headerBtn?.addEventListener('click', () => {
 });
 
 openModalBtn?.addEventListener('click', openModal);
+footerDonate?.addEventListener('click', openFooterModal);
 modalOverlay?.addEventListener('click', closeModal);
+footerModalOverlay?.addEventListener('click', closeFooterModal);
 document.addEventListener('keydown', (e) => {
   if (e.key === 'Escape' && modal.classList.remove('is-open')) {
     closeModal();
+  }
+  if (e.key === 'Escape' && footerModal.classList.remove('is--open')) {
+    closeFooterModal();
   }
 });
